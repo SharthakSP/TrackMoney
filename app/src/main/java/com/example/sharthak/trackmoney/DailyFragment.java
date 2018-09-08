2a71ddbd71bb0b263d9bcabe568fc66d3cd91cc3
Sharthak <sity1c216034@islingtoncollege.edu.np>
2018-09-08 19:18:01 +0545

    First commit

---
+++ app/src/main/java/com/example/sharthak/trackmoney/DailyFragment.java
@@ -0,0 +1,28 @@
+package com.example.sharthak.trackmoney;
+
+
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DailyFragment extends Fragment {
+
+    public DailyFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState) {
+        return inflator.inflate(R.layout.activity_daily_fragment,container,false);
+    }
+}
